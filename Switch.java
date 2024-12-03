d03b7885913100ac9cd713f61af364612d7f7516
Zhi Wei <ozwei51@gmail.com>
2024-12-03 14:32:36 +0800

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Switch {
 
     public static void main(String []args){
@@ -38,5 +40,40 @@ public class Switch {
             break;
     }
     System.out.println("Day of the week: " + dayname);
+    
+
+//Use Scanner to get the day number and the result of the switch must be portrayed in the console.
+
+Scanner scan = new Scanner (System.in);
+System.out.print("Please enter a day number:");
+int dayNumber = scan.nextInt();
+
+switch(dayNumber) {
+    case 1:
+        System.out.println("Monday");
+        break;
+    case 2:
+        System.out.println("Tuesday");
+        break;
+    case 3:
+        System.out.println("Wednesday");
+        break;
+    case 4:
+        System.out.println("Thursday");
+        break;
+    case 5:
+        System.out.println("Friday");
+        break;
+    case 6:
+        System.out.println("Saturday");
+        break;
+    case 7:
+        System.out.println("Sunday");
+        break;
+    default:
+        System.out.println("Invalid day number.");
+}
+
+scan.close();
     }
 }
